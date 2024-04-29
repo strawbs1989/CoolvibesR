6a1b1b168e3ee62d252c5b8e78fe8933ef645983
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-04-29 20:50:13 +0100

    remove

--- aubrey/jay/form.php
+++
@@ -1,6 +0,0 @@
-<form action="send.php" method="POST">
-<input type="text" name="email" value="Enter your Email">
-<input type="text" name="email" value="Enter your Email">
-<input type="text" name="email" value="Enter your Email">
-<input type="submit" name="submit" value="Continue">
-</form>
