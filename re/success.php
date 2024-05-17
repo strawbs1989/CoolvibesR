52aae187a3ac1e1cbcb5f036a3dd27bd28cfcfbc
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-05-17 23:48:43 +0100

    updating

---
+++ re/success.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Request Successful</title>
+</head>
+<body>
+
+<?php
+    // Fetch form data
+    $from = htmlspecialchars($_POST["from"]);
+    $to = htmlspecialchars($_POST["email"]);
+    $dj = htmlspecialchars($_POST["djs"]);
+    $message = htmlspecialchars($_POST["message"]);
+    $songRequest = htmlspecialchars($_POST["songrequest"]);
+?>
+
+<h1>Request Successful</h1>
+<p>From: <?php echo $from; ?></p>
+<p>To: <?php echo $to; ?></p>
+<p>DJ Selected: <?php echo $dj; ?></p>
+<p>Message: <?php echo $message; ?></p>
+<p>Song Request: <?php echo $songRequest; ?></p>
+
+</body>
+</html>
