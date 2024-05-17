808e191f47e2b8f48174b41370975d36686c04f7
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-05-17 13:06:35 +0100

    updating

--- requests/display_requests.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html');
-// Read requests from the flat file
-$file = 'requests/requests.txt';
-$requests = file_get_contents($file);
-
-// Display requests
-echo nl2br($requests); // Convert newlines to <br> tags for HTML display
-?>
