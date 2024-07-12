910ce55703b5929290cdcef7368ccea4b5421ab9
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-07-12 11:58:40 +0100

    removing and adding

--- audio.js
+++
@@ -1,8 +0,0 @@
-function aud_play_pause() {
-                var myAudio = document.getElementById("myAudio");
-                if (myAudio.paused) {
-                    myAudio.play();
-                } else {
-                    myAudio.pause();
-                }
-            }
