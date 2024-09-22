3d46a81089637d56da9188f7f18e7e530eaaa741
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-22 19:07:50 +0100

    Update script.js

--- ReqForm/script.js
+++ ReqForm/script.js
@@ -1,7 +1,8 @@
-script
+<script
   src="https://code.jquery.com/jquery-3.4.1.js"
   integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous">
+  crossorigin="anonymous"></script>
+
 
 function SubForm (){
     $.ajax({
