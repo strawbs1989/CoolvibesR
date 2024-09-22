c8da5e02a0f0e064ca5019284cf26a99b864114e
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-22 18:06:20 +0100

    testing form

---
+++ ReqForm/script.js
@@ -0,0 +1,18 @@
+script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous">
+
+function SubForm (){
+    $.ajax({
+        url:'https://api.apispreadsheets.com/data/7OySATKrFeiUEqMz/',
+        type:'post',
+        data:$("#myForm").serializeArray(),
+        success: function(){
+          alert("Form Submitted Successfully!")
+        },
+        error: function(){
+          alert("Error: Form Not Submitted")
+        }
+    });
+}
