42ecee64c488460f25b8b714172b7e948302fc50
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-27 21:08:12 +0100

    update it

--- ReqForm/ReqQue/display_fetch.js
+++ ReqForm/ReqQue/display_fetch.js
@@ -1,39 +1,58 @@
-fetch("https://api.apispreadsheets.com/data/7OySATKrFeiUEqMz/").then(res=>{
-			if (res.status === 200){
-				res.json().then(data=>{
-					const yourData = data["data"]
-					for(let i = 0; i < yourData.length; i++){
-						let rowInfo = yourData[i]
+fetch("https://api.apispreadsheets.com/data/7OySATKrFeiUEqMz/").then(res => {
+    if (res.status === 200) {
+        res.json().then(data => {
+            const yourData = data["data"];
+            for (let i = 0; i < yourData.length; i++) {
+                let rowInfo = yourData[i];
 
-						let rowInfoDiv = document.createElement("div")
-						rowInfoDiv.classList.add("song-row")
+                let rowInfoDiv = document.createElement("div");
+                rowInfoDiv.classList.add("song-row");
 
-						let rowSong = document.createElement("h2")
-						let rowSongNode = document.createTextNode(rowInfo["Song"])
-						rowSong.appendChild(rowSongNode)
-						rowSong.classList.add("Song")
+                let rowName = document.createElement("h2");
+                let rowNameNode = document.createTextNode(rowInfo["Name"]);
+                rowName.appendChild(rowNameNode);
+                rowName.classList.add("Name");
 
-						let rowArtist = document.createElement("h4")
-						let rowArtistNode = document.createTextNode(rowInfo["Artist"])
-						rowArtist.appendChild(rowArtistNode)
-						rowArtist.classList.add("Artist")
+                let rowSong = document.createElement("h2");
+                let rowSongNode = document.createTextNode(rowInfo["Song"]);
+                rowSong.appendChild(rowSongNode);
+                rowSong.classList.add("Song");
 
-						let rowLink = document.createElement("a")
-						rowLink.setAttribute("href", rowInfo["Link"])
-						rowLink.setAttribute("target","_blank")
-						let rowLinkNode = document.createTextNode(rowInfo["Link"])
-						rowLink.appendChild(rowLinkNode)
-						rowLink.classList.add("Link")
+                let rowArtist = document.createElement("h4");
+                let rowArtistNode = document.createTextNode(rowInfo["Artist"]);
+                rowArtist.appendChild(rowArtistNode);
+                rowArtist.classList.add("Artist");
 
-						rowInfoDiv.appendChild(rowSong)
-						rowInfoDiv.appendChild(rowArtist)
-						rowInfoDiv.appendChild(rowLink)
-						
+                let rowLink = document.createElement("a");
+                rowLink.setAttribute("href", rowInfo["Link"]);
+                rowLink.setAttribute("target", "_blank");
+                let rowLinkNode = document.createTextNode(rowInfo["Link"]);
+                rowLink.appendChild(rowLinkNode);
+                rowLink.classList.add("Link");
 
-						allSongsElm.appendChild(rowInfoDiv)
+                let rowShoutout = document.createElement("p");
+                let rowShoutoutNode = document.createTextNode(rowInfo["Shoutout"]);
+                rowShoutout.appendChild(rowShoutoutNode);
+                rowShoutout.classList.add("Shoutout");
 
-					}
-					
-					loaderElm.style.display = "none"
-					allSongsElm.style.display = "block"
-					errorMessageElm.style.display = "none"
+                rowInfoDiv.appendChild(rowName);
+                rowInfoDiv.appendChild(rowSong);
+                rowInfoDiv.appendChild(rowArtist);
+                rowInfoDiv.appendChild(rowLink);
+                rowInfoDiv.appendChild(rowShoutout);
+
+                allSongsElm.appendChild(rowInfoDiv);
+            }
+
+            loaderElm.style.display = "none";
+            allSongsElm.style.display = "block";
+            errorMessageElm.style.display = "none";
+        }).catch(err => {
+            setErrorDisplay();
+        });
+    } else {
+        setErrorDisplay();
+    }
+}).catch(err => {
+    setErrorDisplay();
+});
