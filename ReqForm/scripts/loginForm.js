c783b012de6d880d829cf25341d4ef995e422ece
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-28 17:25:13 +0100

    Update loginForm.js

--- ReqForm/scripts/loginForm.js
+++ ReqForm/scripts/loginForm.js
@@ -13,23 +13,9 @@ const analytics = getAnalytics(app);
 const auth = firebase.auth();
 
 
-// loginForm.js
-function signIn() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        var user = userCredential.user;
-        console.log("Signed in:", user.email);
-    })
-    .catch((error) => {
-        console.error("Error during sign in:", error.message);
-        alert(error.message);
-    });
-}
-
-
+function signIn(){
+  var email = document.getElementById("email");
+  var password = document.getElementById("password"); 
 
   const promise = auth.signInWithEmailAndPassword(email.value, password.value);
   promise.then(() => location.replace(".https://coolvibes-reloaded.com/ReqForm/ReqQue/djQueue.html")).catch(e => alert(e.message));
@@ -39,7 +25,6 @@ function signIn() {
 
 function signOut(){
   auth.signOut();
-  location.replace("https://coolvibes-reloaded.com/ReqForm/login.html");
+  location.replace(".https://coolvibes-reloaded.com/ReqForm/login.html");
   alert("Signed Out")
-} 
-
+}
