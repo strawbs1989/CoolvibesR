37ca942d5263a980efa6fc57245b4d69ffe785c8
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-28 21:38:17 +0100

    Update api.js

--- ReqForm/ReqQue/js/api.js
+++ ReqForm/ReqQue/js/api.js
@@ -15,14 +15,15 @@ function deleteRow(rowID) {
         alert("Successfully Deleted");
         location.reload();
       } else {
+        console.error("Error Deleting: " + res.status);
         alert("Error Deleting!");
       }
     })
     .catch((err) => {
+      console.error("Error Deleting: ", err);
       alert("Error Deleting!");
     });
 }
-
 
 function updateSongNotes(rowID) {
   const currValueOfNotes = document.getElementById("rowNotesField" + rowID.toString()).value;
@@ -41,24 +42,24 @@ function updateSongNotes(rowID) {
       if (res.status === 201) {
         alert("Notes Updated");
       } else {
-        console.error("Error Updating Notes:", res.status);
+        console.error("Error Updating Notes: " + res.status);
         alert("Notes Not Updated");
       }
     })
     .catch((err) => {
-      console.error("Error Updating Notes:", err);
+      console.error("Error Updating Notes: ", err);
       alert("Error Updating Notes!");
     });
 }
 
-// Fetching the data from the spreadsheet
-fetch("https://api.apispreadsheets.com/data/1-Cf4e1JKN0VMg14vwr34DZRkPd50SraDMUMWsI7KBXE/")
+// Fetching the data from the API
+fetch("https://api.apispreadsheets.com/data/qOnLkupOU7WkHLCD/")
   .then((res) => {
     if (res.status === 200) {
       console.log("Data fetched successfully.");
       return res.json();
     } else {
-      console.error("Error fetching data:", res.status);
+      console.error("Error fetching data: " + res.status);
       setErrorDisplay();
     }
   })
@@ -128,79 +129,6 @@ fetch("https://api.apispreadsheets.com/data/1-Cf4e1JKN0VMg14vwr34DZRkPd50SraDMUM
     errorMessageElm.style.display = "none";
   })
   .catch((err) => {
-    console.error("Error fetching data:", err);
-    setErrorDisplay();
-  });
-
-
-// Fetching the data from the spreadsheet
-fetch("https://api.apispreadsheets.com/data/qOnLkupOU7WkHLCD/")
-  .then((res) => {
-    if (res.status === 200) {
-      return res.json();
-    } else {
-      setErrorDisplay();
-    }
-  })
-  .then((data) => {
-    const yourData = data["data"];
-    for (let i = 0; i < yourData.length; i++) {
-      let rowInfo = yourData[i];
-
-      let rowInfoDiv = document.createElement("div");
-      rowInfoDiv.classList.add("song-row");
-
-      let rowSong = document.createElement("h2");
-      let rowSongNode = document.createTextNode(rowInfo["Song"]);
-      rowSong.appendChild(rowSongNode);
-      rowSong.classList.add("Song");
-
-      let rowArtist = document.createElement("h4");
-      let rowArtistNode = document.createTextNode(rowInfo["Artist"]);
-      rowArtist.appendChild(rowArtistNode);
-      rowArtist.classList.add("Artist");
-
-      let rowLink = document.createElement("a");
-      rowLink.setAttribute("href", rowInfo["Link"]);
-      rowLink.setAttribute("target", "_blank");
-      let rowLinkNode = document.createTextNode(rowInfo["Link"]);
-      rowLink.appendChild(rowLinkNode);
-      rowLink.classList.add("Link");
-
-      let rowDeleteButton = document.createElement("button");
-      rowDeleteButton.setAttribute("onclick", "deleteRow(" + rowInfo["ID"].toString() + ")");
-      let rowDeleteButtonNode = document.createTextNode("Delete Song");
-      rowDeleteButton.appendChild(rowDeleteButtonNode);
-      rowDeleteButton.classList.add("deleteButton");
-
-      let rowNotesField = document.createElement("input");
-      rowNotesField.setAttribute("type", "text");
-      rowNotesField.setAttribute("id", "rowNotesField" + rowInfo["ID"].toString());
-      rowNotesField.setAttribute("value", rowInfo["Notes"]);
-      rowNotesField.classList.add("rowNotesField");
-
-      let rowNotesSaveBtn = document.createElement("button");
-      rowNotesSaveBtn.setAttribute("onclick", "updateSongNotes(" + rowInfo["ID"].toString() + ")");
-      let rowNotesSaveBtnNode = document.createTextNode("Save Notes");
-      rowNotesSaveBtn.appendChild(rowNotesSaveBtnNode);
-      rowNotesSaveBtn.classList.add("rowNotesSaveBtn");
-
-      // Append everything to rowInfoDiv
-      rowInfoDiv.appendChild(rowSong);
-      rowInfoDiv.appendChild(rowArtist);
-      rowInfoDiv.appendChild(rowLink);
-      rowInfoDiv.appendChild(rowNotesField);
-      rowInfoDiv.appendChild(rowNotesSaveBtn);
-      rowInfoDiv.appendChild(rowDeleteButton);
-
-      allSongsElm.appendChild(rowInfoDiv);
-    }
-
-    // Hide loader and display songs
-    loaderElm.style.display = "none";
-    allSongsElm.style.display = "block";
-    errorMessageElm.style.display = "none";
-  })
-  .catch((err) => {
+    console.error("Error fetching data: ", err);
     setErrorDisplay();
   });
