c9a8e35d6d577265f57b008c307384ee245fb3e4
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-28 01:14:43 +0100

    updating

--- ReqForm/loginform.js
+++ ReqForm/loginform.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyAFW1oYgoucS_tFBFpl_m2bAP0FgMair5I",
-    authDomain: "loginform-7bbd8.firebaseapp.com",
-    projectId: "loginform-7bbd8",
-    storageBucket: "buzz-e49ff.appspot.com",
-    messagingSenderId: "405910961497",
+  authDomain: "loginform-7bbd8.firebaseapp.com",
+  projectId: "loginform-7bbd8",
+  storageBucket: "loginform-7bbd8.appspot.com",
+  messagingSenderId: "405910961497",
   appId: "1:405910961497:web:e7b56227b169c89af76048",
   };
   // Initialize Firebase
@@ -18,14 +18,14 @@ function signIn(){
   var password = document.getElementById("password"); 
 
   const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-  promise.then(() => location.replace("./djQueue.html")).catch(e => alert(e.message));
+  promise.then(() => location.replace(".https://coolvibes-reloaded.com/ReqForm/ReqQue/djqueue.html")).catch(e => alert(e.message));
 
 
 };
 
 function signOut(){
   auth.signOut();
-  location.replace("./login.html");
+  location.replace(".https://coolvibes-reloaded.com/ReqForm/loginform.html");
   alert("Signed Out")
 }
 
