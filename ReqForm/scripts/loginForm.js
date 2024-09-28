fccf9843aed8482c903192fe020fc740b6922d1e
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-28 19:09:53 +0100

    update

--- ReqForm/scripts/loginForm.js
+++ ReqForm/scripts/loginForm.js
@@ -1,30 +1,38 @@
-  var firebaseConfig = {
-    apiKey: "AIzaSyAFW1oYgoucS_tFBFpl_m2bAP0FgMair5I",
+// Your Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAFW1oYgoucS_tFBFpl_m2bAP0FgMair5I",
   authDomain: "loginform-7bbd8.firebaseapp.com",
   projectId: "loginform-7bbd8",
   storageBucket: "loginform-7bbd8.appspot.com",
   messagingSenderId: "405910961497",
-  appId: "1:405910961497:web:e7b56227b169c89af76048"
-  };
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const auth = firebase.auth();
-
-
-function signIn(){
-  var email = document.getElementById("email");
-  var password = document.getElementById("password"); 
-
-  const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-  promise.then(() => location.replace(".https://coolvibes-reloaded.com/ReqForm/ReqQue/djQueue.html")).catch(e => alert(e.message));
-
-
+  appId: "1:405910961497:web:e7b56227b169c89af76048",
 };
 
-function signOut(){
-  auth.signOut();
-  location.replace(".https://coolvibes-reloaded.com/ReqForm/login.html");
-  alert("Signed Out")
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Sign In function
+function signIn() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(() => {
+      // Redirect to the DJ Queue page after successful sign in
+      location.replace("https://coolvibes-reloaded.com/ReqForm/ReqQue/djQueue.html");
+    })
+    .catch((e) => {
+      // Alert error message
+      alert(e.message);
+    });
+}
+
+// Sign Out function (in case you need it elsewhere)
+function signOut() {
+  auth.signOut().then(() => {
+    location.replace("https://coolvibes-reloaded.com/ReqForm/login.html");
+    alert("Signed Out");
+  });
 }
