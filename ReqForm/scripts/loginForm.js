c19ac178b09e67481ff57dd1aa6f41d005d924e8
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-09-29 01:47:29 +0100

    update test

--- ReqForm/scripts/loginForm.js
+++ ReqForm/scripts/loginForm.js
@@ -1,11 +1,13 @@
 
-var firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAFW1oYgoucS_tFBFpl_m2bAP0FgMair5I",
   authDomain: "loginform-7bbd8.firebaseapp.com",
   projectId: "loginform-7bbd8",
   storageBucket: "loginform-7bbd8.appspot.com",
   messagingSenderId: "405910961497",
   appId: "1:405910961497:web:e7b56227b169c89af76048",
+  measurementId: "G-KMPQ6X7T1B"
 };
 
 // Initialize Firebase
