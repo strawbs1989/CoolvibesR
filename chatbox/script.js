679fd914b21f50c43c195d77935e6046e21a9380
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-10-16 16:55:06 +0100

    updating again

--- chatbox/script.js
+++ chatbox/script.js
@@ -40,6 +40,7 @@ window.onload = () => {
 
     registerBtn.onclick = async () => {
         const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
         try {
